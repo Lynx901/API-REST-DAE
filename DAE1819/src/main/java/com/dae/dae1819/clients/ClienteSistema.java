313ce81cef92b694707bab8995f355049bb4b758
Daniel Moya <dml00030@red.ujaen.es>
2018-09-26 16:32:51 +0200

    - Añadido menú de consola al cliente

--- DAE1819/src/main/java/com/dae/dae1819/clients/ClienteSistema.java
+++ DAE1819/src/main/java/com/dae/dae1819/clients/ClienteSistema.java
@@ -6,6 +6,7 @@
 package com.dae.dae1819.clients;
 
 import com.dae.dae1819.pojos.Sistema;
+import java.util.Scanner;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -13,16 +14,60 @@ import org.springframework.context.ApplicationContext;
  * @author dml y jfaf
  */
 public class ClienteSistema {
-    
+
     ApplicationContext context;
+
     public ClienteSistema(ApplicationContext context) {
         this.context = context;
     }
-    
+
     public void run() {
         Sistema sistema = (Sistema) context.getBean("sistema");
-        
-        
-        System.out.println("Hola, el cliente se ha iniciado con el servidor: " + sistema.getNombre());
-    }   
-}
+
+        int eleccion = 0;
+
+        do {
+            System.out.print("\n|---------------------------------------------------------------------|" + "\n"
+                    + "|-                                                                   -|" + "\n"
+                    + "|-                            Práctica 1                             -|" + "\n"
+                    + "|-                        Gestión de Eventos                         -|" + "\n"
+                    + "|-                                                                   -|" + "\n"
+                    + "|---------------------------------------------------------------------|" + "\n"
+                    + "|- Seleccione una opción:                                            -|" + "\n"
+                    + "|-                                                                   -|" + "\n"
+                    + "|- [1].Decir nombre del servidor                 sistema.getNombre() -|" + "\n"
+                    + "|---------------------------------------------------------------------|" + "\n"
+                    + "|- [0].Finalizar programa                                            -|" + "\n"
+                    + "|---------------------------------------------------------------------|" + "\n"
+                    + "|-                       (c) 2018 dml y jfaf                         -|" + "\n"
+                    + "|---------------------------------------------------------------------|" + "\n"
+                    + "|- ");
+
+            Scanner capt = new Scanner(System.in);
+            System.out.print("Opción: ");
+            eleccion = capt.nextInt();
+
+            switch (eleccion) {
+                case 1:
+                    System.out.print("|- El servidor se llama " + sistema.getNombre() + ".\n");
+                    break;
+             
+                default:
+                break;
+            }
+            
+            if (eleccion != 0) {
+                capt = new Scanner(System.in);
+                System.out.print("|- ¿Continuar? [Y/N]: ");
+                String end = capt.next();
+
+                if (end.equalsIgnoreCase("n")) {
+                    eleccion = 0;
+                } 
+            }
+            
+      
+        }while (eleccion != 0);
+
+    }
+ }
