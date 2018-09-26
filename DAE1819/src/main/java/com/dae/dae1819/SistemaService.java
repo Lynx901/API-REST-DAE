bd149ec3878b739eebdb63748e99d18564d16b20
Daniel Moya <dml00030@red.ujaen.es>
2018-09-26 12:19:32 +0200

    - Creación jerarquía de clases según diapositivas (v1.0) - Eliminación de archivos de prueba - Movido context.xml a la carpeta config según diapositivas

---
+++ DAE1819/src/main/java/com/dae/dae1819/SistemaService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dae.dae1819;
+
+/**
+ *
+ * @author dml y jfaf
+ */
+
+@SpringBootApplication
+public class SistemaService {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication servidor = new SpringApplication(SistemaService.class);
+        ApplicationContext context = servidor.run(args);
+        
+        ClienteClinica cliente = new ClienteClinica(context);
+
+        cliente.run();
+
+    }
+}
